3105eff6d46ddd536ef9f40b8f0ddde1c6f4dcbb
Yure Moutella <ymoutella@gmail.com>
2025-04-30 17:05:28 -0300

    removed nvims bind

--- config.fish
+++ config.fish
@@ -1,29 +1,7 @@
 if status is-interactive
 
-
     # Commands to run in interactive sessions can go here
 end
-alias ls='exa -l'
-alias nvim-lazy="NVIM_APPNAME=LazyVim nvim"
-alias nvim-kickstart="NVIM_APPNAME=Kickstart nvim"
-alias nvim-astro="NVIM_APPNAME=AstroNvim nvim"
-
-function nvims
-    set items "Kickstart" "LazyVim" "AstroNvim"
-    set config (printf "%s\n" $items | fzf --prompt=" Neovim Config  " --height=~50% --layout=reverse --border --exit-0)
-    
-    if test -z "$config"
-        echo "Nothing selected"
-        return 0
-    else if test "$config" = "default"
-        set config ""
-    end
-    
-    env NVIM_APPNAME=$config nvim $argv
-end
-
-bind \ca 'nvims\n'
-
 
 set -g theme_display_git yes
 set -g theme_display_git_dirty yes
@@ -65,4 +43,8 @@ set -g fish_prompt_pwd_dir_length 0
 set -g theme_project_dir_length 1
 set -g theme_newline_cursor no
 set -g theme_newline_prompt '$ '
+set -gx EDITOR nvim
 
+# bun
+set --export BUN_INSTALL "$HOME/.bun"
+set --export PATH $BUN_INSTALL/bin $PATH
